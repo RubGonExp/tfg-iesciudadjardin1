1c7581564efd47c04ac90496508fe69a92849af2
Ruben Gonzalez <ruben@dawa.io>
2023-06-08 18:35:51 +0200

    commit 02 changes in .deploystack

--- .deploystack/scripts/postinstall.sh
+++ .deploystack/scripts/postinstall.sh
@@ -2,7 +2,7 @@ terraformDIR="terraform/"
 CLIENT=$(terraform -chdir="$terraformDIR" output endpoint)  
 CLIENT=${CLIENT/\"/}
 CLIENT=${CLIENT/\"/}
-echo "Waiting for the client to be active"
+echo "Esperando a que el cliente esté activo"
 
 attempt_counter=0
 max_attempts=50
@@ -10,10 +10,10 @@ max_attempts=50
 until $(curl --output /dev/null --silent --head --fail $CLIENT); do
     if [ ${attempt_counter} -eq ${max_attempts} ];then
     repo=$(git config --get remote.origin.url)
-    echo "Max attempts reached."
-    echo "Solution was not successfully installed."
+    echo "Intentos máximos alcanzados."
+    echo "La solución no se ha instalado correctamente."
     echo 
-    echo "If the problem persists, please file an issue with the Github repo:"
+    echo "Si el problema persiste, envíe una incidencia al repositorio de Github:"
     echo "${repo/.git/}/issues"
     exit 1
     fi
@@ -23,6 +23,6 @@ until $(curl --output /dev/null --silent --head --fail $CLIENT); do
     sleep 5
 done
 
-echo "Success, architecture is ready."
-echo "To see for yourself, go check out:"
+echo "Éxito, la arquitectura está lista."
+echo "Para comprobarlo, visite:"
 echo $CLIENT
